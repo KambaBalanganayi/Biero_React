74c65e5a3aea5915fefdba378eac5ce67d0ff5bc
Kamba Balanganayi <79821137+KambaBalanganayi@users.noreply.github.com>
2023-02-14 13:58:47 -0500

    added comments to describe code functionalities, added a key to the comments section and added courriel as a value within the inputComment and inputRating constants

--- src/composant/Details/Details.js
+++ src/composant/Details/Details.js
@@ -6,6 +6,7 @@ import { IoBeer } from "react-icons/io5";
 
 export default function Details({estConnecte, courriel}){
 
+    //Declaration des constantes du composant Details
     const api_url = "http://127.0.0.1:8000/webservice/php/biere/";
     const {id} = useParams();
 
@@ -16,6 +17,7 @@ export default function Details({estConnecte, courriel}){
     const [inputComment, setCommentValues] = useState([]);
     const [inputRating, setRatingValues] = useState([]);
 
+    //Fetchs des données du composant Détails
     useEffect(()=>{
         fetch(api_url+id)
         .then(data=>data.json())
@@ -43,15 +45,16 @@ export default function Details({estConnecte, courriel}){
     }, []);
 
     // useEffect(()=>{
-    //     console.log(inputRating);
-    // }, [inputRating]);
+    //     setNote(inputRating.note);
+    // }, [inputRating.note]);
 
-    
+    //Fonction de traitement des changements de valeur des champs inputComment(commentaires) et inputRating(note)
     const handleChange = (evt)=>{
-        setCommentValues({...inputComment, [evt.target.name]: evt.target.value});
-        setRatingValues({...inputRating, [evt.target.name]: evt.target.value});
+        setCommentValues({...inputComment, [evt.target.name]: evt.target.value, ['courriel']:courriel});
+        setRatingValues({...inputRating, [evt.target.name]: evt.target.value, ['courriel']:courriel});
     };
 
+    //Logique pour l'ajout d'un commentaire en utilisant l'API biero
     const handleSubmitComment = (evt) => {
         evt.preventDefault();
         let entete = new Headers();
@@ -68,8 +71,11 @@ export default function Details({estConnecte, courriel}){
             .then(data=>{
                 console.log(data);
             })
+            //Utilitaire pour mettre a jour les commentaires apres un ajout
+            setCommentaires([...commentaires, inputComment])
     };
     
+    //Logique pour l'ajout d'une note en utilisant l'API biero
     const handleSubmitRating = (evt) => {
         evt.preventDefault();
         let entete = new Headers();
@@ -88,18 +94,23 @@ export default function Details({estConnecte, courriel}){
             })
     };
 
+    //Initialisation de variables utilises dans le rendu du composant Details
     let message='';
     let formulaireComment="";
     let formulaireRating="";
+
+    //Logique pour l'affichage selon l'etat de connection d'un usager
     if(estConnecte){
         message = <p>Connecté en tant que : {courriel} </p>
+
+        //Formulaire d'ajout d'un commentaire sur une biere
         formulaireComment = <form onSubmit={handleSubmitComment} className="commentForm">
                                 <h3>Ajouter un commentaire : </h3>
-                                <label for="commentaire"></label>
                                 <textarea name='commentaire' onChange={handleChange}></textarea>
                                 <button type='submit'>Ajouter</button>
                             </form>
 
+        //Formulaire d'ajout d'une note sur une biere
         formulaireRating = <form onSubmit={handleSubmitRating} className="ratingForm">
                                 <h3>Ajouter une note : </h3>
                                 <div>
@@ -152,15 +163,15 @@ export default function Details({estConnecte, courriel}){
                 
                 <section className='note'>
                     <strong>Note : </strong>
-                    <p>{note}</p>
+                    <p>{note} / 10</p>
                 </section>
             </section>
         </section>
         <section className='comments'>
             <section className='beerComments'>
             <h3>Commentaires</h3>
-                {commentaires.map(unCommentaire=>
-                    <p><IoBeer/> - {unCommentaire.commentaire}</p>
+                {commentaires.map((unCommentaire, index)=>
+                    <p key={index}><IoBeer/> - {unCommentaire.commentaire}</p>
                 )}
             </section>
 
